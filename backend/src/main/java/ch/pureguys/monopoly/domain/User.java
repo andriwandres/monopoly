56459a3f52a6334820d854c93585736aea12b40e
roman <lysserroman@gmail.com>
2024-10-21 16:10:12 +0200

    R - db example

--- backend/src/main/java/ch/pureguys/monopoly/domain/User.java
+++ backend/src/main/java/ch/pureguys/monopoly/domain/User.java
@@ -5,15 +5,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@Entity
-@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@ToString
+@Entity
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,8 +27,4 @@ public class User {
 
 	private String name;
 
-	public User ()
-	{
-		// Default constructor needed by JPA
-	}
 }
