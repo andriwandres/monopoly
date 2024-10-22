49595fa61f025e844242c7aa79e7b8d8bc469e6e
Dave Luke Eichenberger <dave.eichenberger@stud.hslu.ch>
2024-10-22 15:02:01 +0200

    F - add enum FieldType

---
+++ backend/src/main/java/ch/pureguys/monopoly/domain/FieldType.java
@@ -0,0 +1,14 @@
+package ch.pureguys.monopoly.domain;
+
+public enum FieldType {
+    START,
+    PROPERTY,
+    COMPANY,
+    AIRPORT,
+    TREASURE,
+    SURPRISE,
+    TAX,
+    JAIL,
+    VACATION,
+    GO_TO_JAIL
+}
